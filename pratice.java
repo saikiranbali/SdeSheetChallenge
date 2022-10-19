215506d82c768bb83a4e63b37a632142d2d29a92
saikiranbali <95091708+saikiranbali@users.noreply.github.com>
2022-10-19 08:03:46 +0530

    Update pratice.java

--- pratice.java
+++ pratice.java
@@ -27,3 +27,20 @@ class HelloWorld {
         System.out.println(sum(3));
     }
 }
+
+// recursive program to return the nth fibonocci number
+
+class HelloWorld {
+    public static int fib(int n)
+        {
+            if(n==0)
+            return 0;
+            if(n==1)
+            return 1;
+            return fib(n-1)+fib(n-2);
+        }
+    public static void main(String[] args) {
+      
+        System.out.println(fib(8));
+    }
+}
