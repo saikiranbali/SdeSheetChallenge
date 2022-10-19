0d7a64e9ba65e4b66bfc35ebd0f70279984d8324
saikiranbali <95091708+saikiranbali@users.noreply.github.com>
2022-10-19 08:09:56 +0530

    Update pratice.java

--- pratice.java
+++ pratice.java
@@ -18,7 +18,7 @@ class HelloWorld {
 class HelloWorld {
     public static int sum(int n)
         {
-            if(n==1 || n==1)
+            if(n==0 || n==1)
             return 1;
             return n*sum(n-1);
         }
