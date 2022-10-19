74441908b4883c244158f654bde16c540595dba1
saikiranbali <95091708+saikiranbali@users.noreply.github.com>
2022-10-19 07:58:46 +0530

    Update pratice.java
    updated factorial

--- pratice.java
+++ pratice.java
@@ -12,3 +12,18 @@ class HelloWorld {
         System.out.println(sum(10));
     }
 }
+
+// factorial using recursion
+
+class HelloWorld {
+    public static int sum(int n)
+        {
+            if(n==1 || n==1)
+            return 1;
+            return n*sum(n-1);
+        }
+    public static void main(String[] args) {
+      
+        System.out.println(sum(3));
+    }
+}
